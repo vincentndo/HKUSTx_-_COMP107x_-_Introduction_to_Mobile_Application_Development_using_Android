5961b4e6948c03200e00fd5021e0be19b65e6ec5
Vincent Ninh DO <vincentnhdo@gmail.com>
2018-05-31 23:46:11 -0700

    Done Week 3

---
+++ Week 3 - 2D Graphics/ChatClientColors/app/src/main/java/hk/ust/cse/comp107x/chatclient/Contacts.java
@@ -0,0 +1,57 @@
+package hk.ust.cse.comp107x.chatclient;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Contacts extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    Toolbar toolbar;
+    String[] names;
+
+    ListView friendView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+
+
+        names = getResources().getStringArray(R.array.friends);
+
+        // If you are using a ListView widget, then your activity should implement
+        // the onItemClickListener. Then you should set the OnItemClickListener for
+        // teh ListView.
+        friendView = (ListView) findViewById(R.id.friendListView);
+        friendView.setAdapter(new ArrayAdapter<String>(this, R.layout.friend_item, names));
+        friendView.setOnItemClickListener(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Should set up to add new contacts", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+        Intent mIntent = new Intent(this,ChatClient.class);
+        mIntent.putExtra(getString(R.string.friend), names[position]);
+        startActivity(mIntent);
+
+    }
+}
